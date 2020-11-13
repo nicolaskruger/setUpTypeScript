42218b0a52427d98c1e442695bd87984c20068ec
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-11-13 11:23:36 -0300

    001

--- setUp.bash
+++ setUp.bash
@@ -1,5 +1,24 @@
 #!/bin/bash
+npm init -y
 
+npm install -D typescript@3.3.3
+npm install -D tslint@5.12.1
+
+npm install -S express@4.16.4
+npm install -D @types/express@4.16.1
+
+cat ./setUp/tsconfig.json > ./tsconfig.json
+
+tsc --init
+
+./node_modules/.bin/tslint --init
+
+cat ./setUp/tslint.json > ./tslint.json
+
+cat ./setUp/package.json > ./package.json
+
+
+rm -r ./setUp
 rm -r ./.git
 rm README.md
 rm setUp.bash
